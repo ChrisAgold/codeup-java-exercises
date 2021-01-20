d026ae36c14bc5783be08b1ef873625480d01c31
Chris Agold <christophersagold@gmail.com>
2021-01-20 16:56:42 -0600

    completed exercises

---
+++ src/HelloWorld.java
@@ -0,0 +1,31 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
+        int myFavoriteNumber = 6;
+        System.out.println(myFavoriteNumber);
+
+        // Create a String variable named myString and assign a string value to it, then print the variable out to the console.
+        //String myString = "I like java.";
+        //System.out.println(myString);
+
+        // Change your code to assign a character value to myString. What do you notice?
+        //char myString = 'A';
+        //System.out.println(myString);
+
+        // Change your code to assign the value 3.14159 to myString. What happens?
+        int myString = 657;
+        System.out.println(myString);
+
+        // Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+        // Change your code to assign the value 3.14 to myNumber. What do you notice?
+        long myNumber = 2;
+        System.out.println(myNumber);
+        // Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+
+    }
+}
+
+
+
+
