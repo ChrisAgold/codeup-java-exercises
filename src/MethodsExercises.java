1703aa70c4bd9df1fc7b3dcc348d1a432b394ea1
Chris Agold <christophersagold@gmail.com>
2021-01-30 14:06:49 -0600

    added notes

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -8,70 +8,53 @@ public class MethodsExercises {
         Multiplication(5, 5);
         Division(45, 5);
 
-//        getInteger(1, 10);
-//        System.out.println(MethodsExercises.getInteger(1, 10));
+        getInteger(1, 10);
+        System.out.println(MethodsExercises.getInteger(1, 10));
 
-        factoral(1, 10);
-        System.out.println(MethodsExercises.factoral(1, 10));
+//        factoral(1, 10);
+//        System.out.println(MethodsExercises.factoral(1, 10));
 
     }
 
-    public static void Addition(int a, int b) {
+    public static void Addition(double a, double b) {
         System.out.println(a + b);
     }
-
-    public static void Subtraction(int a, int b) {
+    public static void Subtraction(double a, double b) {
         System.out.println(a - b);
     }
-
-    public static void Multiplication(int a, int b) {
+    public static void Multiplication(double a, double b) {
         System.out.println(a * b);
     }
-
-    public static void Division(int a, int b) {
+    public static void Division(double a, double b) {
         System.out.println(a / b);
     }
 
-//    public static int getInteger(int min, int max) {
+    public static int getInteger(int min, int max) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number between 1 & 10: ");
+        int userInput = input.nextInt();
+        if (userInput < min || userInput > max) {
+            System.out.println("Enter a valid number between 1 & 10!");
+            return getInteger(min, max);
+        } else {
+            System.out.println("Your number is: " + userInput);
+        }
+        return userInput;
+    }
+
+//    public static long factoral(int min, int max) {
 //        Scanner input = new Scanner(System.in);
 //        System.out.print("Enter a number between 1 & 10: ");
-//        int userInput = input.nextInt();
+//        long userInput = input.nextInt();
 //
 //        if (userInput < min || userInput > max) {
 //            System.out.println("Enter a valid number between 1 & 10!");
+//            return factoral(min, max);
 //        } else {
-//            System.out.println("Your number is: " + userInput);
+//            for (int i = 1; i <= max; i++) {
+//                userInput = userInput * min;
+//            }
+//            return userInput;
 //        }
-//        return userInput;
-//    }
-
-
-//    public static int getInteger(int min, int max) {
-//        Scanner input = new Scanner(System.in);
-//        System.out.print("Enter a number between 1 & 10: ");
-//        int userInput = input.nextInt();
-//        if (userInput < min || userInput > max) {
-//            System.out.println("Enter a valid number between 1 & 10!");
-//            return getInteger(min, max);
-//        } else {
-//            System.out.println("Your number is: " + userInput);
-//        }
-//        return userInput;
-//    }
-
-    public static long factoral(int min, int max) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number between 1 & 10: ");
-        long userInput = input.nextInt();
-
-        if (userInput < min || userInput > max) {
-            System.out.println("Enter a valid number between 1 & 10!");
-            return factoral(min, max);
-        } else {
-            for (int i = 1; i <= max; i++) {
-                userInput = userInput * min;
-            }
-            return userInput;
-        }
     }
-}
+
