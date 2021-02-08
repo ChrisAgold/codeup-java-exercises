e3539e7dcdcf4ec137ad92852d00a0967b0870ef
Chris Agold <christophersagold@gmail.com>
2021-02-08 17:43:19 -0600

    added input exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -3,59 +3,83 @@ package util;
 import java.util.Scanner;
 
 public class Input {
+
     private Scanner scanner;
 
-    public Input() {
+    public Input(){
         this.scanner = new Scanner(System.in);
     }
 
-    public String getString() {
+    public String getString(){
         return getString("Please enter a string:");
     }
-
-    public String getString(String prompt) {
+    public String getString(String prompt){
         System.out.println(prompt);
         return this.scanner.nextLine();
     }
 
     public boolean yesNo() {
+        return yesNo("Please enter \"yes\" or \"no\": ");
+    }
+    public boolean yesNo(String prompt){
+        System.out.print(prompt);
+
         String answer = this.scanner.nextLine();
         return (answer.toLowerCase().equals("y") ||
                 answer.toLowerCase().equals("yes"));
     }
 
-    // INT
-    public int getInt() {
-        System.out.printf("Please enter an integer: %n");
-        return this.scanner.nextInt();
+    public int getInt(){
+        return getInt("Please enter an integer: ");
     }
 
-    public int getInt(int min, int max) {
+    public int getInt(String prompt){
+        int number;
+        try{
+            number = Integer.valueOf(getString());
+            return number;
+        }catch(NumberFormatException nfe){
+            System.out.println("Wrong input, try again");
+            return getInt(prompt);
+        }
+
+    }
+
+    public int getInt(int min, int max){
+        return getInt("Please enter an integer between "+min+" and "+max+": ",min,max);
+    }
+    public int getInt(String prompt, int min, int max){
         while (true) {
-            System.out.printf("Please enter an integer between %d & %d: %n", min, max);
+            System.out.println(prompt);
             int answer = this.scanner.nextInt();
             if (answer >= min && answer <= max)
                 return answer;
         }
     }
 
-    // DOUBLE
-    public double getDouble() {
-        System.out.printf("Please enter a double: %n");
-        return this.scanner.nextInt();
+    public double getDouble(){
+        return getDouble("Please enter a double: ");
+    }
+    public double getDouble(String prompt){
+        double number;
+        try {
+            number = Double.valueOf(getString(prompt));
+            return number;
+        } catch (NumberFormatException e){
+            System.out.println("Wrong input, try again");
+            return getDouble(prompt);
+        }
     }
 
-    public double getDouble(double min, double max) {
+    public double getDouble(double min, double max){
+        return getDouble("Please enter a number between "+min+" and "+max+": ",min,max);
+    }
+    public double getDouble(String prompt, double min, double max){
         while (true) {
+            System.out.println(prompt);
             double answer = this.scanner.nextDouble();
             if (answer >= min && answer <= max)
                 return answer;
         }
     }
-
-    public static void main(String[] args) {
-        Input in = new Input();
-    }
-
-
 }
